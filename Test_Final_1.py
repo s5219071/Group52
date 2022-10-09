8f2b016d4a73a90ec82623fd671f443649e49086
s5234269 <110140810+saikatduttatanu@users.noreply.github.com>
2022-10-09 14:45:39 +1000

    Add files via upload

---
+++ Test_Final_1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import unittest
+import Final
+
+class TestFinal(unittest.Testcase):
+    def test_load_dataset(self):
+        result = Final.load_dataset('/Users/saikatduttatanu/Desktop/Coding/SoftwareTechnologies/Project Final/Crash Statistics Victoria.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
